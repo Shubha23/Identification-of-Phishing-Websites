bca806f037f524e20bcae4add7f270dd3602754d
Shubha <17091320+Shubha23@users.noreply.github.com>
2018-09-24 18:21:46 -0400

    Update Classification.py

--- Classification.py
+++ Classification.py
@@ -1,4 +1,10 @@
+'''
+# Aim - Classification of phishing and benign websites.
+# Algorithms applied - Random Forest Classifier, Logistic Regression and SVM
+# Performance metrics - For each model, generates ROC plots, accuracy_score and confusion matrix.
 
+*To do - Apply feature selection to acheive best feature subset
+'''
 import numpy as np  
 import matplotlib.pyplot as plt
 from sklearn.cross_validation import train_test_split
@@ -6,41 +12,57 @@ import pandas as pd
 from sklearn.metrics import accuracy_score,roc_curve,auc, confusion_matrix
 from sklearn import preprocessing
 
-# Read data
+# Read the data
+data = pd.read_csv('./Dataset.csv')
 
-dataframe = pd.read_csv('Dataset.csv')
+# View first and last 5 observations
+print(data.head())
+print(data.tail())
 
-X = np.array(dataframe)[:,0:30]
-y = np.array(dataframe)[:,30]
+# Describe statistical information of data
+print(data.describe())
+# Check column types
+print(data.info())                # All comumns are int type, so no change is required
 
-# Split the data as training and testing data
+# Look for missing values
+print(data.isnull().sum())        # No missing values found, so no need to drop or replace any value
 
-X_train, X_test, y_train, y_test  = train_test_split(X, y, test_size = 0.3, random_state=0)
+# Generate correlation matrix
+print(data.corr())
+
+# Prepare data for models
+X = np.array(data)[:,:30]
+y = np.array(data)[:,30]
+
+# Split the data as training and testing data - 70% train size, 30% test size
+X_train, X_test, y_train, y_test  = train_test_split(X, y, test_size = 0.3, random_state = None)
 
 #1 Classification using Random Forest Classifier
-
 from sklearn.ensemble import RandomForestClassifier
 rfc = RandomForestClassifier()
 rfc = rfc.fit(X_train,y_train)
 prediction = rfc.predict(X_test)
-print(accuracy_score(y_test, prediction))
-fpr,tpr,thresh = roc_curve(y_test,prediction)
-roc_auc = accuracy_score(y_test,prediction)
+print("Accuracy with RF classifier:",accuracy_score(y_test, prediction)) 
+fpr,tpr,thresh = roc_curve(y_test,prediction)      
+roc_auc = accuracy_score(y_test,prediction)         # Calculate ROC AUC
+
+# Plot ROC curve for Random Forest
 plt.plot(fpr,tpr,'g',label = 'Random Forest')
 plt.legend("Random Forest", loc='lower right')
 plt.legend(loc='lower right')
-print (confusion_matrix(y_test,prediction))
+print("Conf matrix RF classifier:",confusion_matrix(y_test,prediction))  #  Generate confusion matrix
 
 #2 Classification using logistic regression
-
 from sklearn.linear_model import LogisticRegression
 logreg = LogisticRegression()
 logreg = logreg.fit(X_train,y_train)
 prediction = logreg.predict(X_test)
-print(accuracy_score(y_test, prediction))
-print (confusion_matrix(y_test,prediction))
+print("Accuracy with Log Reg:", accuracy_score(y_test, prediction))
+print ("Conf matrix Log Reg:",confusion_matrix(y_test,prediction))
 fpr,tpr,thresh = roc_curve(y_test,prediction)
 roc_auc = accuracy_score(y_test,prediction)
+
+# Plot ROC curve for Logistic Regression
 plt.plot(fpr,tpr,'orange',label = 'Logistic Regression')
 plt.legend("Logistic Regression", loc='lower right')
 plt.xlabel("False positive rate")
@@ -48,39 +70,21 @@ plt.ylabel("True positive rate")
 plt.legend(loc='lower right')
 
 #3 Classification using SVM
-
 from sklearn.svm import SVC
-svc_l = SVC(kernel="linear", C=0.025)
+svc_l = SVC(kernel = "linear", C = 0.025)
 svc_l = svc_l.fit(X_train,y_train)
 prediction = svc_l.predict(X_test)
-print(accuracy_score(y_test, prediction))
+print("Accuracy with SVM-Linear:",accuracy_score(y_test, prediction))
 fpr,tpr,thresh = roc_curve(y_test,prediction)
 roc_auc = accuracy_score(y_test,prediction)
+
+# Plot ROC curve for SVM-linear
 plt.plot(fpr,tpr,'b',label = 'SVM')
-plt.legend("SVM", loc='lower right')
-plt.legend(loc='lower right')
-print (confusion_matrix(y_test,prediction))
+plt.legend("SVM", loc ='lower right')
+plt.legend(loc ='lower right')
+print("Conf matrix SVM-linear:",confusion_matrix(y_test,prediction))
 
 plt.show()
-
-#---- FEATURE SELECTION - Recursive Feature Elimination ------------------------
-
-from sklearn.feature_selection import RFE
-rfe = RFE(svc_l,27)
-rfe = rfe.fit(X_train, y_train)
-pred = rfe.predict(X_test)
-print(accuracy_score(y_test,pred))
-
-
-rfe = RFE(rfc,27)
-rfe = rfe.fit(X_train, y_train)
-pred = rfe.predict(X_test)
-print(accuracy_score(y_test,pred))
-
-rfe = RFE(logreg,27)
-rfe = rfe.fit(X_train, y_train)
-pred = rfe.predict(X_test)
-print(accuracy_score(y_test,pred))
 
 
 
